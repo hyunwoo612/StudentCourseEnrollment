a2f252a13bd5de937a236d23ce9383f400fd24dd
hyunwoo612 <sky210120@daum.net>
2025-08-19 15:23:08 +0900

    final

--- src/main/java/com/cocoh/student/repository/EnrollmentRepository.java
+++ src/main/java/com/cocoh/student/repository/EnrollmentRepository.java
@@ -6,6 +6,5 @@ import com.cocoh.student.domain.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment,Long> {
-//    void deleteByEnrollment(String studentId, String courseName, String courseAdvisor);
     Enrollment findByStudentAndCourse(Student student, Course course);
 }
